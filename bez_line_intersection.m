ac64d46209f66146fa8f7534d40e4e6ff5a1e0e9
Zoltán Gyenes <gyezo12@gmail.com>
2019-07-05 16:40:19 +0200

    Intersection is detected

--- bez_line_intersection.m
+++ bez_line_intersection.m
@@ -3,13 +3,14 @@ b0 = (1-t).^2;
 b1 = 2*t.*(1-t);
 b2= t.^2;
 P0 = [1 1]; % point: x-y coordinates
-P1 = [2 3];
-P2 = [3, 2];
+P1 = [2 5];
+P2 = [3, 1];
 
 
 x = P0(1)*b0 +P1(1)*b1 + P2(1)*b2;  
 y = P0(2)*b0 +P1(2)*b1 + P2(2)*b2;
 
+figure(2)
 plot([P0(1),P1(1), P2(1)], [P0(2),P1(2),P2(2)],'bx')
 hold on
 plot(x,y)
@@ -19,10 +20,10 @@ xlabel('x[m]')
 ylabel('y[m]')
 grid on
 hold on
-a = 1
+a = -1
 b= 1
 A = [1,1]; % (a,b)
-c = 3;
+c = 1;
 t_new = 0:0.1:7;
 plot(t_new,-a/A(2)*t_new+c/b,'g')
 
@@ -40,17 +41,25 @@ elojel1 = a*P(1)+ b*P(2)-c
  if isreal(t2)
      t2 = t2(t2<=1)
      t2 = t2(t2>=0)
-     for i=1:size(t2,1)
-         t= t2(i)
+
+         t= t2
         a0 = (1-t).^2;
         a1 = 2*t.*(1-t);
         a2= t.^2;     
         x = P0(1)*a0 +P1(1)*a1 + P2(1)*a2;  
         y = P0(2)*a0 +P1(2)*a1 + P2(2)*a2;
-
+        
         plot(x,y,'ro')
+        
+        tav = sqrt((x-poz(1)).^2 +(y-poz(2)).^2);
+        x = x(find(tav==min(tav)));
+        y = y(find(tav==min(tav)));
+        
+        
+        plot(x,y,'bx')
 
-     end
+
+     
  end
  
  hold on
