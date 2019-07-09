a8411c095f624c27dbe339e35a76cd321f9a92d9
Zoltán Gyenes <gyezo12@gmail.com>
2019-07-09 20:09:47 +0200

    little correction in angle investigation-->abs

--- bez_basic.m
+++ bez_basic.m
@@ -5,12 +5,11 @@ xlim_max = 8;
 ylim_max= 8;
 
 
-
 Pj = [0,-0.5; 3 0; 6 3]
 cp_j(1,3)= 1.5;                    % X2- Kontroll
-cp_j(1,4)= -0.5;                    % Y2- Kontroll
+cp_j(1,4)= -0.5;  
 
-% The number of the segments:
+%The number of the segments:
 number_of_segment = size(Pj,1)-1;
 % the controlpoints of a segment is in one row: pl.: 3 segments--> 3 rows, 1 row: [x1 y1 x2 y2
 % x3y3 ]
@@ -41,17 +40,17 @@ cp_b = ofsetting_bezier_curve(number_of_segment,cp_j,2*bezier_offset,rotation)
 make_corridor_new(cp_j, number_of_segment,t)
 make_corridor_new(cp_k, number_of_segment,t)
 make_corridor_new(cp_b, number_of_segment,t)
-xlim([xlim_min xlim_max])
-ylim([ylim_min ylim_max])
+xlim(xlimits);
+ylim(ylimits);
 grid on
 xlabel('x coordinates [m]')
 ylabel('y coordinates [m]')
 
-% seb = [0.75, 0.5] % the actual velocity vector of the robot (x,y coordinates)
-% poz = [0.5 0] % the actual position of the robot (x,y coordinates)
+seb = [0.75, 0.5] % the actual velocity vector of the robot (x,y coordinates)
+poz = [0.5 0] % the actual position of the robot (x,y coordinates)
+plot(poz(1),poz(2),'bx')
 
-seb = [1.5, 0.9] % the actual velocity vector of the robot (x,y coordinates)
-poz = [1 0.6] % the actual position of the robot (x,y coordinates)
+
 ir_vec = seb-poz
 angle_vel = rad2deg(atan2(seb(2)-poz(2), seb(1)-poz(1)))
 
@@ -62,13 +61,13 @@ b = ir_vec(1)
 c = a*poz(1)+ b*poz(2);
 t_new = -1:0.1:7;
 plot(t_new,-a/b*t_new+c/b,'g')
-
+plot([poz(1) seb(1)], [poz(2) seb(2)],'r')
 % P=P2
 % elojel1 = a*P(1)+ b*P(2)-c
 
         %syms t1;
     %  t = vpasolve( (P0(1)*a+P0(2)*b)*(1-t1)^2 +(P1(1)*a+ P1(2)*b)*2*t1*(1-t1)+ (P2(1)*a+P2(2)*b)*t1^2 -c == 0, t1)
-  cp = cp_k
+  cp = cp_j;
   
   % Az osszes szegmenst meg kell vizsgalni, hogy atmegy-e rajta az egyenes,
   % csak ott kell vizsgalni a metszespontot az egyenessel (2 szomszedos controlpontot behelyettesitve az egyenesbe kül elojelet ad, akkor azon a szegmensen megy at az egyenes)
@@ -103,22 +102,26 @@ plot(t_new,-a/b*t_new+c/b,'g')
               % Meg kell nézni, hogy ha 1 szegmensen belul tobb metszespont
               % van, akkor melyek neznek jo iranyba
               angle_intersection = rad2deg(atan2(y-poz(2), x-poz(1)));
-              x = x((angle_intersection- angle_vel) < 0.0002);
-              y = y((angle_intersection- angle_vel) < 0.0002);
+              x = x(abs(angle_intersection- angle_vel) < 0.0002);
+              y = y(abs(angle_intersection- angle_vel) < 0.0002);
               
               % a metszespontok kozul kiszamitjuk azt, amelyik legkozelebb
-              % van a robotpoziciohoz
+              % van a robotpoziciohoz, ha jo iranyba nez a seb vektor
+              if (~isempty(x))
               tav = sqrt((x-poz(1)).^2 +(y-poz(2)).^2);
               intersection_points(i,:) = [ x(tav==min(tav)) y(tav==min(tav))];
-                                
+              end                 
           end
       end
   end
   % Ahol nin nan, azok a jo metszespontok
   good_segment = find(~isnan(intersection_points(:,1)))
-  
+  if isempty(good_segment)
+      % ekkor nincs metszes az adott gorbe es a poz-seb vektor egyenese
+      % kozott
+  end
   % kul. szegmenseknel milyen tavra van a metsz pont a poz-tol
   segment_intersection_distance = sqrt((intersection_points(good_segment,1)-poz(1)).^2 +(intersection_points(good_segment,2)-poz(2)).^2)
   % kivalasztjuk a metszesek kozul azt, amelyik a legkozelebb van
-  intersection_points(good_segment(find(segment_intersection_distance==min(segment_intersection_distance))),:)
+  metsz = intersection_points(good_segment(find(segment_intersection_distance==min(segment_intersection_distance))),:)
   
